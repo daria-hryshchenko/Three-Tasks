d4637fc86a6e5d9ed721563204227af4623c86b6
daria-hryshchenko <98493900+daria-hryshchenko@users.noreply.github.com>
2023-01-06 13:09:30 +0000

    Deploying to gh-pages from @ daria-hryshchenko/goit-js-hw-09@9d5d2f58849db06adf96d9eec0031117ce37375f 🚀

---
+++ 01-color-switcher.5606b02c.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),d=null;e.disabled=!0,t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,d=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){clearInterval(d),document.body.style.background="",t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.5606b02c.js.map
